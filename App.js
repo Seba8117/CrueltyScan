7200e8e089e51e656ca10386cc2137b0d38033bf
Katherine301 <ka.carrenoa@duocuc.cl>
2022-06-21 15:55:11 -0400

    Se realizan cambios en css de Maquillaje

--- App.js
+++ App.js
@@ -62,7 +62,7 @@ const App = () => {
         <NavigationContainer>
           
           <Stack.Navigator
-            initialRouteName='CuidadoPersonal' //Cambiar pagina 
+            initialRouteName='Maquillaje' //Cambiar pagina 
           >
             <Stack.Screen
               name="Buscamarca"
